79f03da9313ecbe710b24a67d03351e71b21bde0
NOVABASE/NB23152 <carlos.mateus@vodafone.com>
2020-08-07 15:34:09 +0100

    Search feature

--- complex-app/models/Post.js
+++ complex-app/models/Post.js
@@ -96,6 +96,7 @@ Post.reusablePostQuery = function(uniqueOperations, visitorId) {
     // clean up author property in each post object
     posts = posts.map(function(post) {
       post.isVisitorOwner = post.authorId.equals(visitorId)
+      //post.authorId = undefined
 
       post.author = {
         username: post.author.username,
@@ -149,6 +150,20 @@ Post.delete = function(postIdToDelete, currentUserId){
     }catch(e){
       reject()
 
+    }
+  })
+}
+
+Post.search = function(searchTerm) {
+  return new Promise(async (resolve, reject) => {
+    if (typeof(searchTerm) == "string") {
+      let posts = await Post.reusablePostQuery([
+        {$match: {$text: {$search: searchTerm}}},
+        {$sort: {score: {$meta: "textScore"}}}
+      ])
+      resolve(posts)
+    } else { 
+      reject()
     }
   })
 }
